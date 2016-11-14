f6e6393a395301af33b14023cc5ae897717d2323
bchartier <benjamin.chartier@cegetel.net>
2016-11-14 15:48:40 +0100

    Commented unused code removal

--- js/sviewer.js
+++ js/sviewer.js
@@ -7,17 +7,6 @@ proj4.defs([
     ["EPSG:900913", "+title=Web Spherical Mercator, +proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs"],
     ["EPSG:2154", "+title=RGF-93/Lambert 93, +proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
 ]);
-// map projection and grids
-//var projcode = 'EPSG:3857';
-//var projection = ol.proj.get(projcode);
-//var projectionExtent = projection.getExtent();
-//var size = ol.extent.getWidth(projectionExtent) / 256;
-//var resolutions = new Array(20);
-//var matrixIds = new Array(20);
-//for (var z = 0; z < 20; ++z) {
-//    resolutions[z] = size / Math.pow(2, z);
-//    matrixIds[z] = projcode + ':' + z;
-//}
 
 var config = {};
 var customConfig = {};
